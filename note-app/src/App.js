0cefaf385f213a9ed63a163306411f7b81a8a288
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-23 16:33:25 +0100

    main note area added

--- note-app/src/App.js
+++ note-app/src/App.js
@@ -1,12 +1,53 @@
+import { useState } from "react";
 import "./App.css";
+import { nanoid } from "nanoid";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+    const [notes, setNotes] = useState([]); //notes here is passed as a prop Sidebar comp.
+    const [activeNote, setActiveNote] = useState(false);
+
+    const onAddNote = () => {
+        // function for creating a new note
+        const newNote = {
+            // each new not needs to be an boject with obj keys for each note
+            id: nanoid(),
+            title: "Untitled",
+            body: "",
+            lastModified: Date.now(),
+        };
+        setNotes([newNote, ...notes]); //new note created is added to the current notes using ...spread operator
+    };
+
+    const onUpdateNote = (updatedNote) => {
+        const upDatedNotesArray = notes.map((note) => {
+            if (note.id === activeNote) {
+                return updatedNote;
+            }
+            return note;
+        });
+        setNotes(upDatedNotesArray);
+    };
+
+    const onDeleteNote = (idToDelete) => {
+        setNotes(notes.filter((note) => note.id !== idToDelete));
+    };
+
+    const getActiveNote = () => {
+        return notes.find((note) => note.id === activeNote);
+    };
+
     return (
         <div className="App">
-            <Sidebar />
-            <Main />
+            <Sidebar
+                notes={notes}
+                onAddNote={onAddNote}
+                onDeleteNote={onDeleteNote}
+                activeNote={activeNote}
+                setActiveNote={setActiveNote}
+            />
+            <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
         </div>
     );
 }
